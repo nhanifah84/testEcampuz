ed5e527f27f7eb8f236903f34b91527a5065b37c
nhanifah84 <80586194+nhanifah84@users.noreply.github.com>
2022-05-23 13:29:32 +0700

    Hasil Pengerjaan

--- 3_operator_logic.php
+++ 3_operator_logic.php
@@ -2,18 +2,21 @@
 <head>
 </head>
 <body>
-<form name="form1" action="" method="post">
-                Input Angka <input type="text" name="angka1">
-                Input Angka <input type="text" name="angka2">
-                <input type="submit">
-            </form>                
-            <?php  
-            if (isset($_POST["angka1"])) {
-                $angka1 = $_POST["angka1"];
-                $angka2 = $_POST["angka2"];
-                $hasil = $angka1 / $angka2;
-                echo $hasil;                            
-            }          
-            ?>
+    <form name="form1" action="" method="post">
+        Angka 1 <input type="text" value="7">
+        Angka 2 <input type="text" value="2">
+    </form>                
+        <?php 
+        function bagi($a,$b){
+        $x = 0;
+        while($a>1){
+        $a=$a-$b;
+        $x++;
+        }
+        return $x;
+        }
+        $hasil=bagi(7,2);
+        echo $hasil;
+    ?>
 <body>
 </html>
